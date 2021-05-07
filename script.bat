df63634ddee99d829458a42299f9807c3227933d
satwik1806 <53432706+satwik1806@users.noreply.github.com>
2021-05-07 06:08:17 +0530

    Update script.bat
    Echo off, won't print the commands on terminal except the verdict

--- script.bat
+++ script.bat
@@ -3,5 +3,5 @@ FOR /L %%G IN (1,1,7) DO (
     gen %%G > inp.txt
     a < inp.txt > inp1.txt
     b < inp.txt > inp2.txt
-    FC /W C:\Users\satwik\Desktop\CP_Clion\files\inp1.txt C:\Users\satwik\Desktop\CP_Clion\files\inp2.txt > NUL && ECHO same output || ECHO found different output && inp.txt
+    FC /W C:\Users\satwik\Desktop\CP_Clion\files\inp1.txt C:\Users\satwik\Desktop\CP_Clion\files\inp2.txt > NUL && ECHO same output || ECHO found different outputs && inp.txt
 )
