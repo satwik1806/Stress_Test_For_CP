80a25e692a251b59bac30d62316e8910234d2d67
satwik1806 <53432706+satwik1806@users.noreply.github.com>
2021-10-03 13:41:24 +0530

    Update gen.cpp

--- gen.cpp
+++ gen.cpp
@@ -1,8 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifndef ONLINE_JUDGE
+    ~debug() { cerr << endl; }
+    eni(!=) cerr << boolalpha << i; ris; }
+    eni(==) ris << range(begin(i), end(i)); }
+    sim, class b dor(pair < b, c > d) {
+        ris << "(" << d.first << ", " << d.second << ")";
+    }
+    sim dor(rge<c> d) {
+        *this << "[";
+        for (auto it = d.b; it != d.e; ++it)
+            *this << ", " + 2 * (it == d.b) << *it;
+        ris << "]";
+    }
+#else
+    sim dor(const c&) { ris; }
+#endif
+};
+#define print(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
 int randint(int a, int b){
-    return a + rand() % (b - a + 1);
+    uniform_int_distribution<int> uid(a,b);
+    return uid(rng);
 }
 
 char randchar(char a, char b){
@@ -106,12 +138,37 @@ vector<pair<int,int>> gen_graph(int n, int m, bool weight){
     return edges;
 }
 
+vector<int> dis(int n){
+    set<int> temp;
+    vector<int> ret;
+    while(ret.size() != n){
+        int x = randint(1,220);
+        while(temp.find(x) != temp.end()) x = randint(1,220);
+        ret.push_back(x);
+        temp.insert(x);
+    }
+    return ret;
+}
+
 int main(int argc, char* argv[]){
     srand(atoi(argv[1]));
 
-    int n = randint(1,20);
-    vector<pair<int,int>> edges = gen_graph(n,n + 10,0);
-    print_edges(edges,n, n + 10,0);
+    int n = randint(2,20);
+    vector<vector<int>> g(n,vector<int>(5));
+    for(int i=0;i<5;i++){
+        vector<int> temp = dis(n);
+        for(int j=0;j<n;j++){
+            g[j][i] = temp[j];
+        }
+    }
+
+    cout << 1 << endl << n << endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<5;j++){
+            cout << g[i][j] << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
